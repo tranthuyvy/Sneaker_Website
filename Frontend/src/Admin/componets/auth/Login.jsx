4e040dc47d5e6e8bd821126c58454d924861421c
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-10-17 17:47:14 +0700

    toast login

--- Frontend/src/Admin/componets/auth/Login.jsx
+++ Frontend/src/Admin/componets/auth/Login.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Grid, TextField, Button } from "@mui/material";
-// import api from '../../../config/api';
 import axios from "../../../config/axios";
+import { useDispatch, useSelector } from "react-redux";
+import { toast, ToastContainer } from "react-toastify";
+import errorMessagesEn from "../../../Lang/en.json";
+import errorMessagesVi from "../../../Lang/vi.json";
 
 function LoginForm() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const lang = useSelector((state) => state);
+  const errorMessages = lang === "vi" ? errorMessagesVi : errorMessagesEn;
 
   const handleInputChange = (event) => {
     if (event.target.name === "username") {
@@ -18,30 +25,74 @@ function LoginForm() {
   };
 
   const handleSubmit = async (event) => {
+    
     event.preventDefault();
-
+  
     try {
       const response = await axios.post("/api/v1/auth/login", { username, password });
-
+  
       if (response.status === 200) {
         const data = response.data;
-
+        // console.log("code", response.data.code)
+  
         if (data.accessToken) {
           const token = data.accessToken;
           localStorage.setItem("accessToken", token);
-          console.log("token", token)
-          navigate("/admin/staff/profile");
+          
+          const successCode = response.data.code;
+  
+          if (successCode) {
+            toast.success(errorMessages[successCode], {
+              autoClose: 1000,
+            });
+          }
+  
+          dispatch({ type: 'LANG_ENG' });
+          navigate("/admin/staff");
         } else {
-          console.error("Lỗi khi nhận token từ phản hồi.");
+          const errorCode = response.data.code;
+          
+          if (errorCode) {
+            toast.error(errorMessages[errorCode], {
+              autoClose: 1000,
+            });
+            
+          } else {
+            toast.error(lang === "vi" ? "Lỗi khi nhận token từ phản hồi." : "Error receiving token from response", {
+              autoClose: 1000,
+            });
+          }
         }
       } else {
-        console.error("Đăng nhập thất bại");
+        const errorCode = response.data.code;
+        
+        if (errorCode) {
+          toast.error(errorMessages[errorCode], {
+            autoClose: 1000,
+          });
+
+        } else {
+          toast.error(lang === "vi" ? "Đăng nhập thất bại." : "Login failed", {
+            autoClose: 1000,
+          });
+        }
       }
     } catch (error) {
-      console.error("Lỗi khi thực hiện đăng nhập:", error);
+      const errorCode = error.response ? error.response.data.code : null;
+      
+      if (errorCode) {
+        toast.error(errorMessages[errorCode], {
+          autoClose: 1000,
+        });
+
+      } else {
+        toast.error(lang === "vi" ? "Lỗi khi thực hiện đăng nhập." : "Error while logging in", {
+          autoClose: 1000,
+        });
+      }
     }
   };
-
+  
   return (
     <React.Fragment>
       <form style={{ width: "50%" }} onSubmit={handleSubmit}>
@@ -50,7 +101,7 @@ function LoginForm() {
             <TextField
               required
               name="username"
-              label="Tài khoản"
+              label="Account"
               fullWidth
               autoComplete="given-name"
               value={username}
@@ -61,7 +112,7 @@ function LoginForm() {
             <TextField
               required
               name="password"
-              label="Mật khẩu"
+              label="Password"
               fullWidth
               autoComplete="given-name"
               type="password"
@@ -78,11 +129,12 @@ function LoginForm() {
               size="large"
               sx={{ padding: ".8rem 0" }}
             >
-              Đăng nhập
+              {lang === "vi" ? "Đăng nhập" : "Login"}
             </Button>
           </Grid>
         </Grid>
       </form>
+      <ToastContainer />
     </React.Fragment>
   );
 }
