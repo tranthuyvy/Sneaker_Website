d15f60da19ecf1791f1537d5768850e738fea4f5
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-10-19 22:02:24 +0700

    CRUD Supplier

--- Backend/src/controller/supplier_controller.js
+++ Backend/src/controller/supplier_controller.js
@@ -1,96 +1,118 @@
-import Model from '../config/sequelize'
+import Model from "../config/sequelize";
 const supplier = Model.supplier;
 
 class supplier_controller {
-    hello = (req, res) => {
-        return res.send("Hello")
+  hello = (req, res) => {
+    return res.send("Hello");
+  };
+
+  getSupplierById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const foundSupplier = await supplier.findOne({ where: { id } });
+
+      if (foundSupplier) {
+        return res.status(200).json(foundSupplier);
+      } else {
+        return res.status(404).json({ error: "Supplier not found" });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+  createSupplier = async (req, res) => {
+    let { name, address, phone } = req.body;
+
+    if (!name || !address || !phone) {
+      return res.status(500).send({ code: "009" });
+    }
+    name = name.trim();
+    //Lấy id của staff
+    let dataSupplier = await supplier.findOne({ where: { name } });
+    if (dataSupplier && dataSupplier.dataValues && dataSupplier.dataValues.id) {
+      return res.status(500).send({ code: "011" });
     }
 
-    createSupplier = async (req, res) => {
-        let { name, address, phone } = req.body;
-
-        if (!name || !address || !phone) {
-            return res.status(500).send({ code: "009" });
-        }
-        name = name.trim();
-        //Lấy id của staff
-        let dataSupplier = await supplier.findOne({ where: { name } });
-        if (dataSupplier && dataSupplier.dataValues && dataSupplier.dataValues.id) {
-            return res.status(500).send({ code: "011" });
-        }
-
-        try {
-            await supplier.create({ name, address, phone, status: 1 });
-            console.log("Thành công rồi bro");
-            return res.status(200).send({ code: "004" });
-        } catch (e) {
-            console.log(e);
-            return res.status(500).send({ code: "006" });
-        }
-
+    try {
+      await supplier.create({ name, address, phone, status: 1 });
+      console.log("Thành công rồi bro");
+      return res.status(200).send({ code: "004" });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send({ code: "006" });
     }
+  };
 
-    updateSupplier = async (req, res) => {
-        let { id } = req.query;
-        let updataDta = req.body;
-        // if (!name || !address || !phone || !id) {
-        //     return res.status(500).send({ code: "009" });
-        // }
-        if (id) {
-            try {
-                let dataSupplier = await supplier.findOne({ where: { id } });
-                if (dataSupplier && dataSupplier.dataValues && dataSupplier.dataValues.id) {
-                    if (updataDta.name) {
-                        dataSupplier.name = updataDta.name;
-                    }
-                    if (updataDta.address) {
-                        dataSupplier.address = updataDta.address;
-                    }
-                    if (updataDta.phone) {
-                        dataSupplier.phone = updataDta.phone;
-                    }
-                    // await dataSupplier.update({
-                    //     name, address, phone
-                    // })
-                    await dataSupplier.save();
-                    return res.status(200).send({ code: "013" });
-                }
-                else {
-                    return res.status(404).send({ code: "014" });
-                }
-            } catch (e) {
-                console.log(e);
-                return res.status(500).send({ code: "006" });
-            }
+  updateSupplier = async (req, res) => {
+    let { id } = req.query;
+    let updataDta = req.body;
+    // if (!name || !address || !phone || !id) {
+    //     return res.status(500).send({ code: "009" });
+    // }
+    if (id) {
+      try {
+        let dataSupplier = await supplier.findOne({ where: { id } });
+        if (
+          dataSupplier &&
+          dataSupplier.dataValues &&
+          dataSupplier.dataValues.id
+        ) {
+          if (updataDta.name) {
+            dataSupplier.name = updataDta.name;
+          }
+          if (updataDta.address) {
+            dataSupplier.address = updataDta.address;
+          }
+          if (updataDta.phone) {
+            dataSupplier.phone = updataDta.phone;
+          }
+          // await dataSupplier.update({
+          //     name, address, phone
+          // })
+          await dataSupplier.save();
+          return res.status(200).send({ code: "013" });
+        } else {
+          return res.status(404).send({ code: "014" });
         }
+      } catch (e) {
+        console.log(e);
+        return res.status(500).send({ code: "006" });
+      }
     }
+  };
 
-    disableSupplier = async (req, res) => {
-        let id = req.query.id;
-        let checkIdSupplier = await supplier.findOne({ where: { id } });
-        if (checkIdSupplier && checkIdSupplier.dataValues && checkIdSupplier.dataValues.id) {
-            try {
-                await checkIdSupplier.update({ status: 2 });
-                return res.status(200).send({ code: "013" });
-            } catch (e) {
-                console.log(e);
-                return res.status(500).send({ code: "006" });
-            }
-        }
-        else {
-            return res.status(404).send({ code: "014" });
-        }
+  disableSupplier = async (req, res) => {
+    let id = req.query.id;
+    let checkIdSupplier = await supplier.findOne({ where: { id } });
+    if (
+      checkIdSupplier &&
+      checkIdSupplier.dataValues &&
+      checkIdSupplier.dataValues.id
+    ) {
+      try {
+        await checkIdSupplier.update({ status: 0 });
+        return res.status(200).send({ code: "013" });
+      } catch (e) {
+        console.log(e);
+        return res.status(500).send({ code: "006" });
+      }
+    } else {
+      return res.status(404).send({ code: "014" });
     }
+  };
 
-    getAllSupllier = async (req, res) => {
-        try {
-            let data = await supplier.findAll();
-            return res.status(200).send({ code: "002", data })
-        } catch (e) {
-            console.log(e);
-            return res.status(500).send({ code: "006" });
-        }
+  getAllSupllier = async (req, res) => {
+    try {
+      let data = await supplier.findAll();
+      return res.status(200).send({ code: "002", data });
+    } catch (e) {
+      console.log(e);
+      return res.status(500).send({ code: "006" });
     }
+  };
 }
 
 export default new supplier_controller();
--- Backend/src/router/supplier.js
+++ Backend/src/router/supplier.js
@@ -3,8 +3,9 @@ const supplier_router = Router();
 import supplier_controller from "controller/supplier_controller";
 
 supplier_router.get("/get", supplier_controller.getAllSupllier);
-supplier_router.post("/create", supplier_controller.createSupplier)
-supplier_router.put("/update", supplier_controller.updateSupplier)
-supplier_router.put("/disable", supplier_controller.disableSupplier)
+supplier_router.get("/get/:id", supplier_controller.getSupplierById);
+supplier_router.post("/create", supplier_controller.createSupplier);
+supplier_router.put("/update", supplier_controller.updateSupplier);
+supplier_router.put("/disable", supplier_controller.disableSupplier);
 
 export default supplier_router;
