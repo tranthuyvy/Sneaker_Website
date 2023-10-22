3af20bbf5f9a1c14cf476a92a7ad5d4a89e44d08
Phong Ne <PHONGCENA\PhongCENA>
2023-10-22 20:17:35 +0700

    Viết api crud brand và api category

---
+++ Backend/src/router/brand.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import brand_controller from "controller/brand_controller";
+const brand_router = Router();
+
+brand_router.post("/create", brand_controller.createBrand);
+brand_router.put("/update", brand_controller.updateBrand);
+brand_router.put("/disable", brand_controller.disableBrand);
+brand_router.get("/get", brand_controller.getAllBrand);
+export default brand_router;
