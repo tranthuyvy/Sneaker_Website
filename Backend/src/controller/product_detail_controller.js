02278494bf753a0c46e15919914f65bb04bd33a8
Đức Mạnh <ducmanhlai22@gmail.com>
2023-10-28 13:59:46 +0700

    fix cart backend

--- Backend/src/controller/product_detail_controller.js
+++ Backend/src/controller/product_detail_controller.js
@@ -21,7 +21,8 @@ class product_detail_controller {
     async get(req, res) {
         try {
             const id = req.query?.id || 1;
-            const detail = await Model.product_detail.findByPk(id, {
+            const listPro = [];
+            listPro.push(Model.product_detail.findByPk(id, {
                 include: [
                     {
                         model: Model.product,
@@ -36,23 +37,22 @@ class product_detail_controller {
                                 as: "images"
                             }
                         ]
-                    },
-                    {
-                        model: Model.product_batch_item,
-                        as: "product_batch_items",
-                        attributes: [[fn('SUM', literal('product_batch_items.quantity')), 'product_batch_item_quantity']],
-                        group: ['id_product_detail'],
-                    },
-                    {
-                        model: Model.order_detail,
-                        as: "order_details",
-                        attributes: [[fn('SUM', literal('order_details.quantity')), 'order_quantity']],
-                        group: [`${literal('order_details.id_product_detail')}`]
-                    },
-                ],
-            });
-            let batch_quantity = detail?.product_batch_items[0]?.dataValues.product_batch_item_quantity || 0
-            let order_quantity = detail?.order_details[0]?.dataValues.order_quantity || 0
+                    }
+                ]
+            }))
+            listPro.push(Model.product_batch_item.findOne({
+                where: { id_product_detail: id },
+                attributes: [[fn('SUM', literal('quantity')), 'quantity']],
+                group: [`id_product_detail`]
+            }))
+            listPro.push(Model.order_detail.findOne({
+                where: { id_product_detail: id },
+                attributes: [[fn('SUM', literal('quantity')), 'quantity']],
+                group: [`id_product_detail`]
+            }))
+            const [detail, product_batch_item, order_detail] = await Promise.all(listPro);
+            let batch_quantity = product_batch_item?.dataValues?.quantity || 0
+            let order_quantity = order_detail?.dataValues?.quantity || 0
             const data = { ...detail.dataValues, quantity: batch_quantity - order_quantity }
             res.status(200).send({
                 code: '002',
