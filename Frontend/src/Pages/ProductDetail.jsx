71c09c1f334fd521c1831f33bec0950eb0a0e95c
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-10-31 00:48:13 +0700

    add review into Product detail

--- Frontend/src/Pages/ProductDetail.jsx
+++ Frontend/src/Pages/ProductDetail.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState} from "react";
 import { useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
-
 import { RadioGroup } from "@headlessui/react";
-import { Button, Rating } from "@mui/material";
+import { Box, Button, Grid, LinearProgress, Rating, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "../config/axios";
 import { getImage, setCart } from "../config/common";
+import ProductReviewCard from "../Components/ProductReviewCard";
+
 export default function ProductDetails(props) {
   const location = useLocation();
   const { lang, cart } = useSelector((state) => {
@@ -18,6 +19,7 @@ export default function ProductDetails(props) {
   const [discountedPrice, setDiscountedPrice] = useState(0);
   const [discountPersent, setDiscountedPresent] = useState(10);
   const id = location.search.match(/id=(.+)/)[1];
+
   useEffect(() => {
     (async () => {
       const data = (await axios.get(`/api/v1/product/get?id=${id}`)).data;
@@ -44,6 +46,50 @@ export default function ProductDetails(props) {
       }
     })();
   }, []);
+
+  const totalRatings = (product && product.reviews) ? product.reviews.reduce(
+    (total, review) => total + review.star,
+    0
+  ) : 0;
+
+  const averageRating = totalRatings / product?.reviews.length;
+
+  console.log("averageRating", averageRating)
+
+  // total ratings excellent
+  const excellentRatings = (product?.reviews || []).filter(
+    (review) => review.star === 4.5 || review.star === 5
+  );
+  const totalExcellentRatings = excellentRatings.length;
+
+  // total ratings very good
+  const veryGoodRatings = (product?.reviews || []).filter(
+    (review) => review.star === 3.5 || review.star === 4
+  );
+  const totalVeryGoodRatings = veryGoodRatings.length;
+
+  //total ratings good
+  const goodRatings = (product?.reviews || []).filter(
+    (review) => review.star === 2.5 || review.star === 3
+  );
+  const totalGoodRatings = goodRatings.length;
+
+  //total ratings avarage
+  const averageRatings = (product?.reviews || []).filter(
+    (review) => review.star === 1.5 || review.star === 2
+  );
+  const totalAverageRatings = averageRatings.length;
+
+  //total ratings poor
+  const poorRatings = (product?.reviews || []).filter(
+    (review) => review.star === 0.5 || review.star === 1 || review.star === 0
+  );
+  const totalPoorRatings = poorRatings.length;
+
+  console.log("product", product);
+  console.log("product Review", product?.reviews);
+  console.log("product Review", product?.reviews.length);
+
   return (
     <div className="bg-white lg:px-20">
       <div className="pt-6">
@@ -88,7 +134,7 @@ export default function ProductDetails(props) {
             <div className="overflow-hidden rounded-lg max-w-[30rem] max-h-[35rem] border">
               <img
                 src={product ? getImage(product) : null}
-                alt={"Hình ảnh giày"}
+                alt={"Product Image"}
                 loading={"lazy"}
                 className="h-full w-full object-cover object-center"
               />
@@ -112,10 +158,10 @@ export default function ProductDetails(props) {
           {/* product detail */}
           <div className="lg:col-span-1 mx-auto max-w-2xl px-4 pb-16 sm:px-6  lg:max-w-7xl  lg:px-8 lg:pb-24">
             <div className="lg:col-span-2">
-              <h1 className="text-lg lg:text-xl font-semibold tracking-tight text-black-700">
+              <h1 className="text-lg lg:text-xl font-semibold tracking-tight text-black-700 mb-1">
                 {product?.name}
               </h1>
-              <h1 className="text-sm tracking-tight text-gray-900 opacity-60 pt-1">
+              <h1 className="text-sm tracking-tight text-gray-900 opacity-60 pt-1 mb-1">
                 {product?.id_branch_branch.name}
               </h1>
             </div>
@@ -146,11 +192,11 @@ export default function ProductDetails(props) {
                 <h3 className="sr-only">Reviews</h3>
 
                 <div className="flex items-center space-x-3">
-                  <Rating name="read-only" value={6} precision={0.5} readOnly />
+                  <Rating name="read-only" value={averageRating} precision={0.5} readOnly />
 
-                  <p className="opacity-60 text-sm">{5} Ratings</p>
+                  <p className="opacity-60 text-sm">{product?.reviews.length} Ratings</p>
                   <p className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                    {5} reviews
+                    {product?.reviews.length} reviews
                   </p>
                 </div>
               </div>
@@ -206,9 +252,20 @@ export default function ProductDetails(props) {
                 <Button
                   variant="contained"
                   onClick={addCart}
-                  sx={{ padding: ".8rem 2rem", marginTop: "2rem" }}
+                  sx={{
+                    padding: ".8rem 2rem",
+                    marginTop: "2rem",
+                    backgroundColor: "#9155FD",
+                    borderRadius: 2,
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "#6f38e6",
+                    },
+                  }}
                 >
-                  Add To Cart
+                  <Typography variant="button" fontWeight="bold">
+                    Add To Cart
+                  </Typography>
                 </Button>
               </form>
             </div>
@@ -239,22 +296,215 @@ export default function ProductDetails(props) {
             </div>
           </div>
         </section>
+
+        {/* {/* rating and review section */}
+        <section className="">
+          <h1 className="font-semibold text-lg mb-3">
+            Recent Review & Ratings
+          </h1>
+
+          <div className="border p-3">
+            <Grid container spacing={7}>
+              <Grid item xs={7}>
+                <div className="space-y-5">
+                  {product?.reviews.map((item, i) => (
+                    <ProductReviewCard item={item} />
+                  ))}
+                </div>
+              </Grid>
+
+              <Grid item xs={5}>
+                <h1 className="text-xl font-semibold mb-3">Product Ratings</h1>
+                <div className="flex items-center space-x-3 pb-1">
+                  <Rating
+                    name="read-only"
+                    value={averageRating}
+                    precision={0.5}
+                    readOnly
+                  />
+
+                  <p className="opacity-60">
+                    {product?.reviews.length} Ratings
+                  </p>
+                </div>
+                <Box>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={2}
+                  >
+                    <Grid xs={2}>
+                      <p className="p-0">Excellent</p>
+                    </Grid>
+                    <Grid xs={7}>
+                      <LinearProgress
+                        className=""
+                        sx={{ bgcolor: "#d0d0d0", borderRadius: 4, height: 7 }}
+                        variant="determinate"
+                        value={
+                          (totalExcellentRatings /
+                            product?.reviews.length) * 100
+                        }
+                        color="success"
+                      />
+                    </Grid>
+                    <Grid xs={2}>
+                      <p className="opacity-50 p-2">
+                        {totalExcellentRatings} Ratings
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Box>
+
+                <Box>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={2}
+                  >
+                    <Grid xs={2}>
+                      <p className="p-0">Very Good</p>
+                    </Grid>
+                    <Grid xs={7}>
+                      <LinearProgress
+                        className=""
+                        sx={{ bgcolor: "#d0d0d0", borderRadius: 4, height: 7 }}
+                        variant="determinate"
+                        value={
+                          (totalVeryGoodRatings /
+                            product?.reviews.length) * 100
+                        }
+                        color="success"
+                      />
+                    </Grid>
+                    <Grid xs={2}>
+                      <p className="opacity-50 p-2">
+                        {totalVeryGoodRatings} Ratings
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Box>
+
+                <Box>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={2}
+                  >
+                    <Grid xs={2}>
+                      <p className="p-0">Good</p>
+                    </Grid>
+                    <Grid xs={7}>
+                      <LinearProgress
+                        className="bg-[#885c0a]"
+                        sx={{ bgcolor: "#d0d0d0", borderRadius: 4, height: 7 }}
+                        variant="determinate"
+                        value={
+                          (totalGoodRatings /
+                            product?.reviews.length) * 100
+                        }
+                        color="warning"
+                      />
+                    </Grid>
+                    <Grid xs={2}>
+                      <p className="opacity-50 p-2">
+                        {totalGoodRatings} Ratings
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Box>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={2}
+                  >
+                    <Grid xs={2}>
+                      <p className="p-0">Avarage</p>
+                    </Grid>
+                    <Grid xs={7}>
+                      <LinearProgress
+                        className=""
+                        sx={{
+                          bgcolor: "#d0d0d0",
+                          borderRadius: 4,
+                          height: 7,
+                          "& .MuiLinearProgress-bar": {
+                            bgcolor: "#885c0a",
+                          },
+                        }}
+                        variant="determinate"
+                        value={
+                          (totalAverageRatings /
+                            product?.reviews.length) *
+                          100
+                        }
+                        color="warning"
+                      />
+                    </Grid>
+                    <Grid xs={2}>
+                      <p className="opacity-50 p-2">
+                        {totalAverageRatings} Ratings
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Box>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={2}
+                  >
+                    <Grid xs={2}>
+                      <p className="p-0">Poor</p>
+                    </Grid>
+                    <Grid xs={7}>
+                      <LinearProgress
+                        className=""
+                        sx={{ bgcolor: "#d0d0d0", borderRadius: 4, height: 7 }}
+                        variant="determinate"
+                        value={
+                          (totalPoorRatings /
+                            product?.reviews.length) *
+                          100
+                        }
+                        color="error"
+                      />
+                    </Grid>
+                    <Grid xs={2}>
+                      <p className="opacity-50 p-2">
+                        {totalPoorRatings} Ratings
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+            </Grid>
+          </div>
+        </section>
       </div>
     </div>
   );
+
   function addCart() {
     let listCart = [];
     listSize.forEach((item) => {
-      if (item.isChecked && !checkInCart(item.id)) listCart.push({ ...item, quantity: 1 });
+      if (item.isChecked && !checkInCart(item.id))
+        listCart.push({ ...item, quantity: 1 });
     });
-    dispatch({ type: "SET_CART", data: [...cart,...listCart] });
-    setCart([...cart,...listCart]);
-    toast(lang['020'])
+    dispatch({ type: "SET_CART", data: [...cart, ...listCart] });
+    setCart([...cart, ...listCart]);
+    toast(lang["020"]);
   }
-  function checkInCart(id){
-    for(let i of cart){
-       if (i.id.localeCompare(id)==0) return true
+  function checkInCart(id) {
+    for (let i of cart) {
+      if (i.id.localeCompare(id) == 0) return true;
     }
-    return false
+    return false;
   }
 }
