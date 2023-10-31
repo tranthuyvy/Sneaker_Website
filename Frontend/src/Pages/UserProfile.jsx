96110620d9540e87a98cb4dba2f9c3b94a9528e0
Đức Mạnh <ducmanhlai22@gmail.com>
2023-11-01 00:06:21 +0700

    add profile

--- Frontend/src/Pages/UserProfile.jsx
+++ Frontend/src/Pages/UserProfile.jsx
@@ -8,35 +8,39 @@ import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import axiosApiInstance from "../config/api";
 import { useSelector } from "react-redux";
 import { toast, ToastContainer } from "react-toastify";
+import { Select, MenuItem } from "@mui/material";
 import errorMessagesEn from "../Lang/en.json";
 import errorMessagesVi from "../Lang/vi.json";
-import { Select, MenuItem } from "@mui/material";
+import "../Styles/UserDetail.css";
 function UserProfile() {
   const [user, setUser] = useState();
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
   const [delay, setDelay] = useState(Math.random() * 4000 + 500);
+  const [isEditing, setIsEditing] = useState(false);
   useEffect(() => {
-    async function call() {
+    function call() {
       getUser()
         .then()
         .catch((err) => {
           setTimeout(() => {
-            call().catch((err) => {});
-          },delay);
+            call();
+          }, delay);
         });
     }
-    call().then().catch();
+    call();
   }, []);
   return (
-    <Fragment>
+    <Fragment className='h-screen'>
       <Typography
         variant="h3"
         sx={{ textAlign: "center" }}
         className="py-10 text-center"
       >
-        {/* Hello, {editedStaff.name} */}
+        Hello, {user?.name}
         <div className="flex justify-center m-10">
           <label htmlFor="fileInput">
-            {/* <Avatar
+            <Avatar
               className="text-white"
               aria-haspopup="true"
               sx={{
@@ -48,16 +52,14 @@ function UserProfile() {
                 fontSize: "30px",
               }}
             >
-              {avatarImage ? (
+              {user?.avatar ? (
                 <img
-                  src={avatarImage}
+                  src={user.avatar}
                   alt="Selected Avatar"
                   style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
-              ) : (
-                editedStaff.name.charAt(0)
-              )}
-            </Avatar> */}
+              ) : null}
+            </Avatar>
           </label>
           <input
             id="fileInput"
@@ -68,141 +70,76 @@ function UserProfile() {
           />
         </div>
       </Typography>
-      {/* <form className="updateProfileContainer min-h-screen">
+      <div className="h-44 px-20">
         <Grid container spacing={2}>
-          {[
-            {
-              label: "Name",
-              name: "name",
-              value: editedStaff.name,
-            },
-            {
-              label: "Email",
-              name: "email",
-              value: editedStaff.email,
-            },
-            {
-              label: "CCCD",
-              name: "id_card",
-              value: editedStaff.id_card,
-            },
-            {
-              label: "Phone",
-              name: "phone",
-              value: editedStaff.phone,
-            },
-            // {
-            //   label: "Birthday",
-            //   name: "date_of_birth",
-            //   type: Date,
-            //   // value: editedStaff.date_of_birth
-            //   value: format(new Date(editedStaff.date_of_birth), "dd/MM/yyyy")
-            // },
-            {
-              label: "Bank Number",
-              name: "bank_account_number",
-              value: editedStaff.bank_account_number,
-            },
-            {
-              label: "Start Work",
-              name: "start_work",
-              value: format(new Date(editedStaff.start_work), "dd/MM/yyyy"),
-              disabled: true,
-            },
-            {
-              label: "Status",
-              name: "status",
-              //   value: editedStaff.status,
-              disabled: true,
-              startAdornment: (
-                <InputAdornment position="start">
-                  {editedStaff.status === 0 ? (
-                    <>
-                      <FiberManualRecordIcon style={{ color: "green" }} />
-                      Làm Việc
-                    </>
-                  ) : (
-                    <>
-                      <FiberManualRecordIcon style={{ color: "red" }} />
-                      Ngừng Làm Việc
-                    </>
-                  )}
-                </InputAdornment>
-              ),
-            },
-          ].map((field, index) => (
-            <Grid item xs={12} sm={4} key={index}>
-              <TextField
-                fullWidth
-                label={field.label}
-                name={field.name}
-                value={field.value}
-                onChange={handleChange}
-                type={field.type || "text"}
-                disabled={!isEditing || field.disabled}
-                InputProps={
-                  field.startAdornment && {
-                    startAdornment: field.startAdornment,
-                  }
-                }
-              />
-            </Grid>
-          ))}
-          <Grid item xs={12} sm={4}>
-            <Select
+          <Grid item xs={12} sm={4} key={1}>
+            <span>Name</span>
+            <TextField
               fullWidth
-              label="Gender"
-              name="sex"
-              value={gender}
-              onChange={handleGenderChange}
-              disabled={!isEditing}
-            >
-              <MenuItem value="Nam">Nam</MenuItem>
-              <MenuItem value="Nữ">Nữ</MenuItem>
-            </Select>
+              placeholder={"name"}
+              name={"name"}
+              value={name}
+              onChange={handleChange}
+              type={"text"}
+            />
           </Grid>
-
+          <Grid item xs={12} sm={4} key={2}>
+            <span>Email</span>
+            <TextField
+              fullWidth
+              placeholder={"email"}
+              name={"email"}
+              value={user?.email}
+              onChange={handleChange}
+              type={"text"}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={12} sm={4} key={3}>
+            <span>Phone</span>
+            <TextField
+              fullWidth
+              placeholder={"phone"}
+              name={"phone"}
+              value={phone}
+              onChange={handleChange}
+              type={"text"}
+            />
+          </Grid>
           <Grid item xs={12}>
-            {isEditing ? (
-              <>
-                <Button
-                  variant="contained"
-                  sx={{ p: 1.8 }}
-                  className="py-20"
-                  size="large"
-                  onClick={handleSaveEdit}
-                >
-                  Save Changes
-                </Button>
-                <Button
-                  variant="contained"
-                  sx={{ p: 1.8, ml: 2 }}
-                  className="py-20"
-                  size="large"
-                  onClick={handleCancelEdit}
-                >
-                  Cancel
-                </Button>
-              </>
-            ) : (
-              <Button
-                variant="contained"
-                sx={{ p: 1.8 }}
-                className="py-20"
-                size="large"
-                onClick={handleEdit}
-              >
-                Update Profile
-              </Button>
-            )}
+            <Button
+              variant="contained"
+              sx={{ p: 1.8 }}
+              className="py-20"
+              size="large"
+              // onClick={handleEdit}
+            >
+              Update Profile
+            </Button>
           </Grid>
         </Grid>
         <ToastContainer />
-      </form> */}
+      </div>
     </Fragment>
   );
   async function getUser() {
     const data = (await axiosApiInstance.get("api/v1/user")).data.data;
+    setUser(data);
+    setName(data?.name || "");
+    setPhone(data?.phone || "");
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "phone":
+        setPhone(value);
+        break;
+      default:
+        break;
+    }
   }
 }
 
