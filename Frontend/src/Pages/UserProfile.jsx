eb2394fa0b880832fa0c11a0cd2f1aa676ff867a
Đức Mạnh <ducmanhlai22@gmail.com>
2023-11-02 00:09:14 +0700

    fgfgf

--- Frontend/src/Pages/UserProfile.jsx
+++ Frontend/src/Pages/UserProfile.jsx
@@ -3,154 +3,46 @@ import { InputAdornment, Typography } from "@mui/material";
 import { Grid, TextField, Button, Avatar } from "@mui/material";
 import { deepPurple } from "@mui/material/colors";
 import { Fragment } from "react";
-import { format } from "date-fns";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
 import axiosApiInstance from "../config/api";
 import { useSelector } from "react-redux";
 import { toast, ToastContainer } from "react-toastify";
-import { Select, MenuItem } from "@mui/material";
-import errorMessagesEn from "../Lang/en.json";
-import errorMessagesVi from "../Lang/vi.json";
+import OrderUser from "../Components/OrderUser";
+import AddressUser from "../Components/AddressUser";
 import "../Styles/UserDetail.css";
+import InforUser from "../Components/InforUser";
 function UserProfile() {
-  const [user, setUser] = useState();
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [delay, setDelay] = useState(Math.random() * 4000 + 500);
-  const [isEditing, setIsEditing] = useState(false);
-  useEffect(() => {
-    function call() {
-      getUser()
-        .then()
-        .catch((err) => {
-          setTimeout(() => {
-            call();
-          }, delay);
-        });
-    }
-    call();
-  }, []);
+  const [listItem, setListItem] = useState([
+    { name: "Info", isActive: true, component: <InforUser></InforUser> },
+    { name: "Address", isActive: false, component: <AddressUser/> },
+    { name: "Order", isActive: false, component: <OrderUser/> },
+  ]);
+  const [item, setItem] = useState(listItem.find((item) => item.isActive));
+  useEffect(()=>{
+ 
+  },[])
+  function handleLi(item){
+    setItem(item)
+    setListItem(listItem.map(i=>{
+      return i.name.localeCompare(item.name)==0 ? {...i,isActive:true} : {...i,isActive:false}
+    }))
+  }
   return (
-    <Fragment className="h-screen">
-      <Typography
-        variant="h3"
-        sx={{ textAlign: "center" }}
-        className="py-10 text-center"
-      >
-        Hello, {user?.name}
-        <div className="flex justify-center m-10">
-          <label htmlFor="fileInput">
-            <Avatar
-              className="text-white"
-              aria-haspopup="true"
-              sx={{
-                bgcolor: deepPurple[500],
-                color: "white",
-                cursor: "pointer",
-                width: "100px",
-                height: "100px",
-                fontSize: "30px",
-              }}
-            >
-              {user?.avatar ? (
-                <img
-                  src={user.avatar}
-                  alt="Selected Avatar"
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
-              ) : null}
-            </Avatar>
-          </label>
-          <input
-            id="fileInput"
-            type="file"
-            accept="image/*"
-            style={{ display: "none" }}
-            // onChange={handleImageChange}
-          />
-        </div>
-      </Typography>
-      <div className="h-44 px-10 flex flex-row">
-        <div className="w-1/3 h-44">
-          <span>Dashboard</span>
-          <ul>
-            <li>Info</li>
-            <li>Address</li>
-            <li>Order</li>
+
+    <Fragment className="max-h-52">
+      <div className="px-10 flex flex-row pt-10">
+        <div className="w-1/3 h-44 justify-center font-bold">
+          <div className="border-b-4 w-full h-1/3 text-2xl">Dashboard</div>
+          <ul className="h-2/3">
+            {listItem.map((item) => {
+              return <li onClick={()=>{handleLi(item)}} className={`${item.isActive ? 'text-rose-600': 'text-black'} border-b-4 w-full h-1/3 `}>{item.name}</li>;
+            })}
           </ul>
         </div>
-        <div className="w-2/3 h-44">
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={4} key={1}>
-              <span>Name</span>
-              <TextField
-                fullWidth
-                placeholder={"name"}
-                name={"name"}
-                value={name}
-                onChange={handleChange}
-                type={"text"}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4} key={2}>
-              <span>Email</span>
-              <TextField
-                fullWidth
-                placeholder={"email"}
-                name={"email"}
-                value={user?.email}
-                onChange={handleChange}
-                type={"text"}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={12} sm={4} key={3}>
-              <span>Phone</span>
-              <TextField
-                fullWidth
-                placeholder={"phone"}
-                name={"phone"}
-                value={phone}
-                onChange={handleChange}
-                type={"text"}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                variant="contained"
-                sx={{ p: 1.8 }}
-                className="py-20"
-                size="large"
-                // onClick={handleEdit}
-              >
-                Update Profile
-              </Button>
-            </Grid>
-          </Grid>
-          <ToastContainer />
-        </div>
+        {item?.component}
       </div>
     </Fragment>
   );
-  async function getUser() {
-    const data = (await axiosApiInstance.get("api/v1/user")).data.data;
-    setUser(data);
-    setName(data?.name || "");
-    setPhone(data?.phone || "");
-  }
-  function handleChange(event) {
-    const { name, value } = event.target;
-    switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "phone":
-        setPhone(value);
-        break;
-      default:
-        break;
-    }
-  }
 }
 
 export default UserProfile;
